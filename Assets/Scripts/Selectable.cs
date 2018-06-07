4ffd165b5ae169a6a335b7d9b3236262e400719d
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-06-07 10:44:55 -0400

    Selecting building layer in progress

---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.layer = LayerMask.NameToLayer("building");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit)
+            {
+
+                //if (hitInfo.transform.gameObject.layer == "building")
+                //{
+                //    Debug.Log("It's working!");
+                //}
+                //else
+                //{
+                //    Debug.Log("not working");
+                //}
+            }
+        }
+
+    }
+}
