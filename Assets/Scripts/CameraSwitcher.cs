1999b04707b7e2b1ee555ff3ea36c0c1263bac17
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-06-11 11:16:07 -0400

    Changes

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject flyCamera;
+    public GameObject g;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainCamera.enabled = true;
+        flyCamera.SetActive(false);
+        
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            flyCamera.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            flyCamera.SetActive(false);
+        }
+	}
+}
